c7982ce8f300c24276a597f00d8957de558b7ef8
gogotanaka <mail@tanakakazuki.com>
2014-10-09 20:42:31 -0700

    Remove unused internal func from internal.h

--- internal.h
+++ internal.h
@@ -701,17 +701,7 @@ VALUE rb_get_expanded_load_path(void);
 NORETURN(void rb_load_fail(VALUE, const char*));
 
 /* math.c */
-VALUE rb_math_atan2(VALUE, VALUE);
-VALUE rb_math_cos(VALUE);
-VALUE rb_math_cosh(VALUE);
-VALUE rb_math_exp(VALUE);
-VALUE rb_math_hypot(VALUE, VALUE);
 VALUE rb_math_log(int argc, const VALUE *argv);
-VALUE rb_math_sin(VALUE);
-VALUE rb_math_sinh(VALUE);
-#if 0
-VALUE rb_math_sqrt(VALUE);
-#endif
 
 /* newline.c */
 void Init_newline(void);
